853ae5032f34508f22bdca9abca010896ff58a28
silan.wu <silan.wu@shopee.com>
2022-11-01 15:59:52 +0800

    init ts and memo

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,10 +1,11 @@
-import { createSignal, createEffect } from "./reactive";
+import { createSignal, createEffect, createMemo } from "./reactive";
 
 import "./index.css";
 
 export function Home() {
   console.log("1. Create Signal");
   const [getCount, setCount] = createSignal(1);
+  const [name, setName] = createSignal("silan");
 
   console.log("2. Create Reaction");
   createEffect(() => {
@@ -16,10 +17,21 @@ export function Home() {
 
   const add = () => {
     setCount(getCount() + 1);
+    setName("xx");
   };
   const clear = () => {
     setCount(0);
   };
+  const sum = createMemo(() => {
+    if (name() === "silan") {
+      return name() + " is " + getCount() * 3;
+    }
+    return getCount();
+  }, 0);
+  createEffect(() => {
+    console.log("sum", sum());
+  });
+
   return (
     <div class="root">
       <div style={{ "margin-bottom": "20px" }}>mysolid</div>
--- src/pages/Home/reactive.ts
+++ src/pages/Home/reactive.ts
@@ -1,34 +1,44 @@
-//@ts-nocheck
-const effectStack = [];
-function subscribe(runningEffect, subs) {
-  subs.add(runningEffect);
-  runningEffect.deps.add(subs);
-}
-function cleanup(runningEffect) {
-  for (const dep of runningEffect.deps) {
-    dep.delete(runningEffect);
-  }
-  runningEffect.deps.clear();
+interface Effect {
+  execute: () => void;
+  deps?: Set<Set<Effect>>;
 }
 
-export function createSignal(value) {
-  const subs = new Set();
+type Getter<T> = () => T;
+type Setter<T> = (newValue: T) => any;
+
+const effectStack: Effect[] = [];
+
+function subscribe(runningEffect: Effect, observers: Set<Effect>) {
+  observers.add(runningEffect);
+  runningEffect.deps?.add(observers);
+}
+function cleanup(runningEffect: Effect) {
+  if (runningEffect.deps) {
+    for (const dep of runningEffect.deps) {
+      dep.delete(runningEffect);
+    }
+    runningEffect.deps?.clear();
+  }
+}
+
+export function createSignal<T>(value: T): [Getter<T>, Setter<T>] {
+  const observers = new Set<Effect>();
   const getter = () => {
     const effect = effectStack[effectStack.length - 1];
     if (effect) {
-      subscribe(effect, subs);
+      subscribe(effect, observers);
     }
     return value;
   };
-  const setter = (newValue) => {
+  const setter = (newValue: T) => {
     value = newValue;
-    for (const sub of [...subs]) {
+    for (const sub of [...observers]) {
       sub.execute();
     }
   };
   return [getter, setter];
 }
-export function createEffect(callback) {
+export function createEffect(callback: (args?: any) => any) {
   const execute = () => {
     cleanup(effect);
     effectStack.push(effect);
@@ -38,6 +48,12 @@ export function createEffect(callback) {
       effectStack.pop();
     }
   };
-  const effect = { execute, deps: new Set() };
+  const effect = { execute };
   execute();
+}
+
+export function createMemo<T>(callback: () => T, initValue: T) {
+  const [get, set] = createSignal<T>(initValue);
+  createEffect(() => set(callback()));
+  return get;
 }
